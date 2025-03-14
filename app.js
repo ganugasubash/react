61457fbd42ef2bf79182a16cc7e1086b8dbc9b36
sneakershub03 <shafi.cse.rymec@gmail.com>
2025-03-14 11:16:37 +0530

    commit 1st

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-/*const heading = React.createElement(
-    "h1" ,
-    { id:"heading"},
-    "hello world react" 
-); */
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent=React.createElement("div",{id: "parent"},[
     React.createElement("div",{ id:"child"}, [
         React.createElement("h1",{ }, "i'm an h1 tag"),
@@ -14,9 +10,7 @@ const parent=React.createElement("div",{id: "parent"},[
         React.createElement("h2", {}, "i'm an h2 tag"),
     ]),
 ]);
-
-
 console.log(parent);
 const root= ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-  
+root.render(parent); 
+
